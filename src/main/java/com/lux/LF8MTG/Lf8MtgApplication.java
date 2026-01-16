46f14268cc675e66525a2b6531e8b989418f4bd2
Aurora Lux <auroraluxpruss@gmail.com>
2026-01-16 11:58:50 +0100

    Initial Spring and ERM push

---
+++ src/main/java/com/lux/LF8MTG/Lf8MtgApplication.java
@@ -0,0 +1,13 @@
+package com.lux.LF8MTG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lf8MtgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lf8MtgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lux/LF8MTG/Lf8MtgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lux.LF8MTG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lf8MtgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
